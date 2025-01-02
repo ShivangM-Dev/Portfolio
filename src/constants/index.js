9e092ac7dc5133a924be2441951f9c7daff6a2a4
Shivang Mandal <shivangmdev@gmail.com>
2025-01-03 00:24:04 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,220 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Emily Johnson',
+    position: 'Marketing Director at GreenLeaf',
+    img: 'assets/review1.png',
+    review:
+      'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+  },
+  {
+    id: 2,
+    name: 'Mark Rogers',
+    position: 'Founder of TechGear Shop',
+    img: 'assets/review2.png',
+    review:
+      'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+  },
+  {
+    id: 3,
+    name: 'John Dohsas',
+    position: 'Project Manager at UrbanTech ',
+    img: 'assets/review3.png',
+    review:
+      'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+  },
+  {
+    id: 4,
+    name: 'Ether Smith',
+    position: 'CEO of BrightStar Enterprises',
+    img: 'assets/review4.png',
+    review:
+      'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'Car Hub',
+    desc: 'Car Hub is a premier online platform for buying, selling, and renting cars. With a user-friendly interface, it connects car enthusiasts, buyers, and sellers in one convenient hub. Whether you\'re looking to purchase your dream car, sell a pre-owned vehicle, or rent a car for short-term needs, Car Hub offers seamless solutions tailored to your preferences. The website features a vast inventory of new and used cars, competitive rental options, and secure payment systems. Advanced search filters and personalized recommendations ensure you find the perfect match quickly. Trust Car Hub for reliable, transparent, and hassle-free car transactions.',
+    subdesc:
+      'Your Ultimate Destination for Cars\n' +
+      'At Car Hub, we simplify car ownership and rentals. Explore a wide range of vehicles, compare prices, and connect with trusted sellers or renters. Whether buying, selling, or renting, Car Hub is your one-stop shop for all things automotive.',
+    href: 'https://carhub-lilac-two.vercel.app',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Apple',
+    href: 'https://iphon3-d.vercel.app/',
+    desc: 'Experience the elegance of Apple’s design in an interactive 3D environment with our Apple Website Clone using Three.js. This project brings Apple’s signature sleek aesthetics and user-friendly navigation to life with dynamic 3D elements and animations powered by Three.js. From immersive product showcases to smooth scroll-triggered effects, every detail is crafted to replicate Apple’s iconic digital presence.',
+    subdesc:
+      'Key features include responsive layouts, realistic product models, and seamless performance across devices. Whether you’re exploring this as a personal project, design inspiration, or a creative learning tool, this clone combines modern web technologies to deliver an engaging user experience.',
+
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.jpg',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Three Js',
+        path: '/assets/threejs.png',
+      },
+    ],
+  },
+  {
+    title: 'Zentry',
+    desc: 'Visually Captivating Website Inspired by Zentry  \n' +
+      '\n' +
+      'Immerse yourself in a visually stunning digital experience with a website inspired by Zentry’s design philosophy. This creative masterpiece combines scroll-triggered animations, smooth geometric transitions, and compelling video storytelling to captivate users at every interaction. Designed to evoke curiosity and deliver a seamless user journey, the site blends bold visuals, dynamic motion effects, and interactive elements that respond fluidly to user actions. Perfect for brands or projects seeking to leave a memorable impression, this design celebrates innovation and creativity in web development, ensuring an engaging and immersive experience for all visitors.',
+    subdesc:
+      'visually captivating website inspired by Zentry, featuring scroll-triggered animations, geometric transitions, and engaging video storytelling.',
+    href: 'https://gamingwebsite-murex.vercel.app/',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'GSAP',
+        path: '/assets/gsap.jpg',
+      },
+    ],
+  },
+
+
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Framer',
+    pos: 'Lead Web Developer',
+    duration: '2022 - Present',
+    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
+    icon: '/assets/framer.svg',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Figma',
+    pos: 'Web Developer',
+    duration: '2020 - 2022',
+    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
+    icon: '/assets/figma.svg',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Notion',
+    pos: 'Junior Web Developer',
+    duration: '2019 - 2020',
+    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
+    icon: '/assets/notion.svg',
+    animation: 'salute',
+  },
+];
